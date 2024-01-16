4997eecb686ce196f1e3ac2ff1481e01fa31f919
tinbane <tinbane@tinbane.com>
2024-01-15 16:15:16 -0800

    first commit

--- js/sketch.js
+++ js/sketch.js
@@ -4,9 +4,5 @@ function setup() {
 }
 
 function draw() {
-  background(0, 20, 20);
-  fill(220, 50, 90);
-  ellipse(width * 0.5, height * 0.5, width * 0.25);
-  fill(90, 80, 70);
-  ellipse(width * 0.5, height * 0.5, width * 0.15);
+  background(0, 90, 10);
 }
